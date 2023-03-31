cd89f1af098e58f417ecd7029cebcd7fe9631e7c
shrastaniraula <shrastatsarhs58@gmail.com>
2023-03-31 12:02:24 +0545

    sign up complete

--- lib/Signup.dart
+++ lib/Signup.dart
@@ -12,79 +12,140 @@ class Signup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Signup Page'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Name',
-              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8.0),
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter your name',
+      backgroundColor: Color.fromARGB(255, 228, 218, 254),
+      body: SingleChildScrollView(
+        child: Container(
+          child: SafeArea(
+            child: Column(children: [
+              Text("SIGN UP",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 60,
+                      color: Color.fromARGB(255, 79, 49, 162),
+                      fontWeight: FontWeight.w500)),
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Email',
-              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8.0),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter your email',
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'Name',
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 220, 207, 254),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                        hintText: 'Enter your name',
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'Email',
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 220, 207, 254),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                        hintText: 'Enter your email',
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'Password',
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 220, 207, 254),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                        hintText: 'Enter your password',
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'Confirm Password',
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    TextField(
+                      controller: confirmPasswordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 220, 207, 254),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                        hintText: 'Confirm your password',
+                      ),
+                    ),
+                    SizedBox(height: 22.0),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Password',
-              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8.0),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter your password',
+              GestureDetector(
+                onTap: () {
+                  // Perform signup logic here
+                  print('Name: ${nameController.text}');
+                  print('Email: ${emailController.text}');
+                  print('Password: ${passwordController.text}');
+                  print('Confirm Password: ${confirmPasswordController.text}');
+                },
+                child: Container(
+                  width: 250,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Color.fromARGB(255, 79, 49, 162),
+                  ),
+                  child: Center(
+                      child: Text('Sign Up',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Color.fromARGB(246, 255, 255, 255)))),
+                ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Confirm Password',
-              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8.0),
-            TextField(
-              controller: confirmPasswordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Confirm your password',
-              ),
-            ),
-            SizedBox(height: 32.0),
-            ElevatedButton(
-              onPressed: () {
-                // Perform signup logic here
-                print('Name: ${nameController.text}');
-                print('Email: ${emailController.text}');
-                print('Password: ${passwordController.text}');
-                print('Confirm Password: ${confirmPasswordController.text}');
-              },
-              child: Text('Sign up'),
-            ),
-          ],
+              SizedBox(height: 10.0),
+              Text("Already have an account? Log in")
+            ]),
+          ),
         ),
       ),
     );
